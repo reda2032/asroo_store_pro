c63079ef5d64dce712586abbdd2620df812ae978
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-07-25 23:55:38 +0300

    env_create done

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'asroo_store_app.dart';
+import 'core/app/env.variables.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
