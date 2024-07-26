e8ab4344f1878bb7c2134ee42cfa5d7463f341fe
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-07-26 12:14:34 +0300

    ScreenUtilInit done

--- lib/asroo_store_app.dart
+++ lib/asroo_store_app.dart
@@ -1,6 +1,7 @@
 import 'package:asroo_store/core/app/connectivity_controller.dart';
 import 'package:asroo_store/core/common/screens/no_network_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class AsrooStoreApp extends StatelessWidget {
   const AsrooStoreApp({super.key});
@@ -11,28 +12,32 @@ class AsrooStoreApp extends StatelessWidget {
       valueListenable: ConnectivityController.instance.isConnected,
       builder: (_, value, __) {
         if (value) {
-          return MaterialApp(
-            title: 'Asroo Store',
-            debugShowCheckedModeBanner: false,
-            //  debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            builder: (context, widget) {
-              return Scaffold(
-                body: Builder(
-                  builder: (context) {
-                    ConnectivityController.instance.init();
-                    return widget!;
-                  },
+          return ScreenUtilInit(
+            designSize: const Size(375, 812),
+            minTextAdapt: true,
+            child: MaterialApp(
+              title: 'Asroo Store',
+              debugShowCheckedModeBanner: false,
+              //  debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              builder: (context, widget) {
+                return Scaffold(
+                  body: Builder(
+                    builder: (context) {
+                      ConnectivityController.instance.init();
+                      return widget!;
+                    },
+                  ),
+                );
+              },
+              home: Scaffold(
+                appBar: AppBar(
+                  centerTitle: true,
+                  title: const Text('Asroo Store'),
                 ),
-              );
-            },
-            home: Scaffold(
-              appBar: AppBar(
-                centerTitle: true,
-                title: const Text('Asroo Store'),
               ),
             ),
           );
