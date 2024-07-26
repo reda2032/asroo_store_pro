1680d4b2f469b4b3d0bb20a276a5e487814f6d21
reda_ahmed <reda.ahmed.2032@gmail.com>
2024-07-26 19:38:08 +0300

    Routes and Navigator done

--- lib/features/splash/presentation/views/widgets/splash_screen_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_screen_body.dart
@@ -1,5 +1,7 @@
+import 'package:asroo_store/core/extensions/context_extension.dart';
 import 'package:flutter/material.dart';
 
+import '../../../../../core/routes/app_routes.dart';
 import '../../../../../core/style/images/app_images.dart';
 
 class SplashScreenBody extends StatefulWidget {
@@ -18,20 +20,22 @@ class _SplashScreenBodyState extends State<SplashScreenBody> {
 
   @override
   Widget build(BuildContext context) {
-    return const Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Image(
-          image: AssetImage(AppImages.splashScreen),
-          fit: BoxFit.cover,
+    return Scaffold(
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(AppImages.splashScreen),
+            fit: BoxFit.fill,
+          ),
         ),
-      ],
+      ),
     );
   }
 
   void excuteNaviagtion() {
     Future.delayed(const Duration(seconds: 6), () {
+      context.pushName(AppRoutes.testOne);
       //   Navigator.pushReplacementNamed(context, SigninView.routeName);
     });
   }
---
+++ lib/test_one_screen.dart
@@ -0,0 +1,31 @@
+import 'package:asroo_store/core/extensions/context_extension.dart';
+import 'package:flutter/material.dart';
+
+import 'core/routes/app_routes.dart';
+
+class TestOneScreen extends StatelessWidget {
+  const TestOneScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.pink,
+      appBar: AppBar(
+        title: const Text('One'),
+      ),
+      body: Center(
+        child: TextButton(
+          onPressed: () {
+            context.pushName(AppRoutes.testTwo);
+          },
+          child: const Text(
+            'Go Two Screen',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
